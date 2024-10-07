618c43bdfea2bf279267ae474859e0acef404f81
francis.ferdinard <francis.ferdinard@PROGRAM1>
2024-10-06 22:26:26 -0500

    Add project files.

---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting(options => {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = true;
+            });
+
+            services.AddControllersWithViews();
+
+            services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
+        }
+
+
+        // Use this method to configure the HTTP request pipeline.
+
+        //public void Configure(IApplicationBuilder app)
+        //{
+        //    app.UseDeveloperExceptionPage();
+        //    app.UseHttpsRedirection();
+        //    app.UseStaticFiles();
+
+        //    app.UseRouting();
+        //    app.UseEndpoints(endpoints =>
+        //    {
+        //        endpoints.MapControllerRoute(
+        //            name: "default",
+        //            pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
+        //    });
+        //}
+
+    }
+}
